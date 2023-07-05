1bd63b3f15e59e5b52b8354b10e60fae0c822c03
hunggahot <hunggahot@gmail.com>
2023-07-05 19:43:11 +0700

    Prettier Format

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from '~/pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Route>
+
+        </Route>
+      </div>
+    </Router>
   );
 }
 
